2e29d49d7331ffc56df18ce0117a15e27efae354
Tom Deadman <tom.deadman@lagoni.co.uk>
2019-01-29 14:22:10 +0000

    updates

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
-
-_ "src/github.com/mattn/go-sqlite3"
+	_ "src/github.com/mattn/go-sqlite3"
 )
+
+type SearchResult struct{
+	Title string
+	Author string
+	Year string
+	ID string
+}
 type Page struct{
 	Name string
 	DBStatus bool
@@ -30,6 +37,17 @@ func main() {
 
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil{
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request){
+		results := []SearchResult{
+			SearchResult{"Moby-Dick", "Herman Welville", "1851", "2222" },
+		}
+
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(results); err != nil {
+			http.Error(w,err.Error(), http.StatusInternalServerError)
 		}
 	})
 	fmt.Println(http.ListenAndServe(":8081", nil))
